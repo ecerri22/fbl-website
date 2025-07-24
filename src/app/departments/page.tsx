ebda501ceb331d265c0f3f060f60cd7c8b03a816
Enia Cerri <ecerri22@epoka.edu.al>
2025-07-24 22:33:20 +0200

    pagewrapper in departments page

--- src/app/departments/page.tsx
+++ src/app/departments/page.tsx
@@ -3,6 +3,7 @@
 import Link from "next/link";
 import Image from "next/image";
 import { use } from "react";
+import PageWrapper from "@/components/PageWrapper";
 
 
 const departments = [
@@ -24,32 +25,34 @@ export default function DepartmentPage({ params }: { params: Promise<{ slug: str
   const { slug } = use(params);
 
   return (
-    <div className="max-w-6xl mx-auto px-4 py-12">
-      <h1 className="text-3xl font-bold text-blue-950 mb-8">Departamentet</h1>
+    <PageWrapper>
+      <main className="max-w-6xl mx-auto px-4 py-12">
+        <h1 className="text-3xl font-bold text-blue-950 mb-8">Departamentet</h1>
 
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        {departments.map((dept) => (
-          <Link
-            key={dept.slug}
-            href={`/departments/${dept.slug}`}
-            className="border rounded-lg overflow-hidden shadow-md hover:shadow-lg transition block"
-          >
-            <Image
-              src={dept.image}
-              alt={dept.name}
-              width={800}
-              height={300}
-              className="w-full h-40 object-cover"
-            />
-            <div className="p-4">
-              <h2 className="text-xl font-semibold text-blue-900">
-                {dept.name}
-              </h2>
-              <p className="text-gray-700">{dept.description}</p>
-            </div>
-          </Link>
-        ))}
-      </div>
-    </div>
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+          {departments.map((dept) => (
+            <Link
+              key={dept.slug}
+              href={`/departments/${dept.slug}`}
+              className="border rounded-lg overflow-hidden shadow-md hover:shadow-lg transition block"
+            >
+              <Image
+                src={dept.image}
+                alt={dept.name}
+                width={800}
+                height={300}
+                className="w-full h-40 object-cover"
+              />
+              <div className="p-4">
+                <h2 className="text-xl font-semibold text-blue-900">
+                  {dept.name}
+                </h2>
+                <p className="text-gray-700">{dept.description}</p>
+              </div>
+            </Link>
+          ))}
+        </div>
+      </main>
+    </PageWrapper>
   );
 }
